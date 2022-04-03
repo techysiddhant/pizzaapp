63d22d65cc78ba7a2208df8930b8f92282cfddd0
techysiddhant <sidjain8000@gmail.com>
2022-04-03 23:51:52 +0530

    add cart page, and auth

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 //assests
 app.use(express.static('public'));
-//create a first route
-app.get('/', (req, res) => {
-    // console.log(__dirname, '/resources/views');
-    res.render('home')
-})
+
 
 
 // set tamplate engine
@@ -18,7 +14,23 @@ app.use(expressLayouts);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
 
-
+//create a first route
+app.get('/', (req, res) => {
+        // console.log(__dirname, '/resources/views');
+        res.render('home')
+    })
+    // cart route
+app.get('/cart', (req, res) => {
+        res.render('customers/cart');
+    })
+    //login route
+app.get('/login', (req, res) => {
+        res.render('auth/login');
+    })
+    //register route
+app.get('/register', (req, res) => {
+    res.render('auth/register');
+})
 
 
 // create a server
