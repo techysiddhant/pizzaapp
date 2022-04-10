fbbe69d588e9c77998f865195864d1fa7e8b3602
techysiddhant <sidjain8000@gmail.com>
2022-04-10 23:07:30 +0530

    add to cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,31 @@
-console.log('Hello, world!');
+import axios from 'axios';
+import Noty from 'noty';
+let addToCart = document.querySelectorAll('.add-to-cart');
+let cartCounter = document.querySelector('#cartCounter');
+
+function updateCart(pizza) {
+    axios.post('/update-cart', pizza).then(res => {
+        // console.log(res);
+        cartCounter.innerText = res.data.totalQty;
+        new Noty({
+            type: 'success',
+            timeout: 1000,
+            text: "Item added to the Cart",
+            // progressBar: false
+        }).show();
+    }).catch(err => {
+        new Noty({
+            type: 'error',
+            timeout: 2000,
+            text: "Somwthing went Wrong",
+            progressBar: false
+        }).show();
+    })
+}
+addToCart.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        let pizza = JSON.parse(btn.dataset.pizza);
+        updateCart(pizza);
+        // console.log(pizza);
+    })
+})
--- server.js
+++ server.js
@@ -1,39 +1,63 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const ejs = require('ejs');
-const expressLayouts = require('express-ejs-layouts');
-const path = require('path');
+const ejs = require("ejs");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
 const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("express-flash");
+const MongoDbStore = require("connect-mongo")
+    // database connection
+const url = "mongodb://localhost:27017/pizza";
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection
+    .once("open", () => {
+        console.log("Database connected...");
+    })
+    .on("error", (error) => {
+        console.log("Connection failed...");
+    });
+// session-store
+// let mongoStore = new MongoDbStore({
+//     mongooseConnection: connection,
+//     collection: 'sessions',
+// });
+//session - config
+app.use(
+    session({
+        secret: process.env.COOKIE_SECRET,
+        resave: false,
+        store: MongoDbStore.create({
+            mongoUrl: 'mongodb://localhost:27017/pizza'
+        }),
+        saveUninitialized: false,
+        cookie: { maxAge: 1000 * 60 * 60 * 24 }, //24 hours
+        // cookie: { maxAge: 1000 * 15 }, //15 sec
+    })
+);
+
+app.use(flash());
 //assests
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.json());
 
-
-
-// set tamplate engine
+// Global Middleware
+app.use((req, res, next) => {
+        res.locals.session = req.session;
+        next();
+    })
+    // set tamplate engine
 app.use(expressLayouts);
-app.set('views', path.join(__dirname, '/resources/views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
 
-//create a first route
-app.get('/', (req, res) => {
-        // console.log(__dirname, '/resources/views');
-        res.render('home')
-    })
-    // cart route
-app.get('/cart', (req, res) => {
-        res.render('customers/cart');
-    })
-    //login route
-app.get('/login', (req, res) => {
-        res.render('auth/login');
-    })
-    //register route
-app.get('/register', (req, res) => {
-    res.render('auth/register');
-})
-
+// import the routes
+require("./routes/web")(app);
 
 // create a server
 app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`);
-})
+});
